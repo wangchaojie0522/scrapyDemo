b27054b081b9e31ba8bfa8793bf1844d764ab63e
chaojiewang <chaojiewang@deepglint.com>
2023-11-27 11:30:32 +0800

    【待提交】

--- main.py
+++ main.py
@@ -2,17 +2,25 @@
 Author: chaojiewang chaojiewang@deepglint.com
 Date: 2023-08-10 10:56:15
 LastEditors: chaojiewang chaojiewang@deepglint.com
-LastEditTime: 2023-08-10 11:19:01
+LastEditTime: 2023-08-29 17:55:29
 FilePath: /mySpider/main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
-from scrapy.cmdline import execute
 
-import os 
-import sys
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-# 如果要运行其他的网络爬虫，只需修改上面代码中字符串里面的命令即可 
-execute(["scrapy", "crawl", "first_spider"," -o"," info.csv"])
-#或者
-# cmdline.execute("scrapy crawl itcast -o info.csv -t csv".split()) 
+import matplotlib.pyplot as plt
+from pylab import mpl  
+import numpy as np
+import pandas as pd
+import tushare as ts
+mpl.rcParams['font.sans-serif'] = ['SimHei'] # 用来正常显示中文标签 
+mpl.rcParams['axes.unicode_minus']=False  # 用来正常显示负号
+ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000',
+                periods=1000))
+ts = ts.cumsum()
+ts.plot(figsize=(12,8))
+#利用tushare包抓取股票数据并画图
+#得到的是DataFrame的数据结构
+df=ts.get_k_data('sh',start='1990-01-01')
+df.index=pd.to_datetime(df['date'])
+df['close'].plot(figsize=(12,8))
+plt.title("上证指数走势")
